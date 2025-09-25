60f8ab45bc6c3af81cf6d6b074b51e315c7052d9
= <=>
2025-09-25 12:50:16 +0500

    still working on file reading and searching

--- src/filehandling/file_reader.py
+++ src/filehandling/file_reader.py
@@ -1,35 +1,35 @@
 import os
 from docx import Document
 from pypdf import PdfReader
+from file_searcher import FIleSearcher
+
 
 class FIleReader:
 
-    def read(self,filename:str)->str:
+    def read(self,filepath:str,ext:str)->str:
         PLAIN_EXTS = ['txt','md','log','py','js','html','css','java',"env"]
-        ext = self.extract_ext(filename)
-        file_path = os.path.join(os.getcwd(),filename)
+        # file_path = os.path.join("/home/hasnain/D/code/python/rag_pj",filename)
 
-        print(f"File to read: {filename} with extension: {ext} and path: {file_path}")
+        print(f"File to read: {filepath}")
         if ext in PLAIN_EXTS:
-            return self.read_plain(file_path)
+            return self.read_plain(filepath)
         elif ext == 'pdf':
-            return self.read_pdf(file_path)
+            return self.read_pdf(filepath)
         elif ext == 'docx':
-            return self.read_docx(file_path)
+            return self.read_docx(filepath)
         else:
             print(f"Error:Unsupported file extension: {ext}")
             # raise ValueError(f"Unsupported file extension: {ext}")
     
-    def extract_ext(self,filename:str)->str:
-        return filename.split('.')[-1].lower()
 
+    ## diff file readers ... fro each extension/type
     def read_plain(self,file_path: str) -> str:
         try:
             with open(file_path, 'r', encoding='utf-8') as file:
                 return file.read()
         except Exception as e:
             print(f"Error reading plain text file:\n{e}")
-            return None
+            return e
 
     def read_pdf(self,file_path: str) -> str:
         try: 
@@ -40,20 +40,33 @@ class FIleReader:
             return text
         except Exception as e:
             print(f"Error reading PDF file.\n{e}")
-            return None
+            return e
 
     def read_docx(self,file_path: str) -> str:
         try:
             doc = Document(file_path)
             text = ''
             for para in doc.paragraphs:
-                text += para.text + '\n'
+                if para.text != "":
+                    text += para.text + '\n'
             return text
         except Exception as e:
             print(f"Error reading DOCX file.\n{e}")
-            return None
+            return e
         
 
+if __name__ == "__main__":
+    fs =FIleSearcher()
+    filename = input("Enter filename to search: ")
+    dir = input("Enter directory to search in (or leave blank for '/home/{user}/*'): ")
+    print(f"Searching files...\nFilename: {filename} ||     Dir: {dir}\n")
+    files= fs.search_files(filename,dir)
+    print(f"Found {len(files)} files.")
+    print(files)
 
-class FIleSearcher:
-    
+    if len(files) == 0:
+        exit(0)
+    filepath = files[0].get("path")
+    fr = FIleReader()
+    pdf  = fr.read(filepath,files[0].get("ext"))
+    print(pdf.split("\n")[0:100])
--- src/filehandling/file_searcher.py
+++ src/filehandling/file_searcher.py
@@ -2,30 +2,51 @@ from pathlib import Path
 
 
 class FIleSearcher:
-    def __init__(self, directory: str):
-        self.directory = Path(directory)
 
-    def search_files(self, query: str):
+
+    def extract_ext(self,filename:str)->str:
+        return filename.split('.')[-1].lower()
+
+    def search_files(self, filename: str ,dir:str = ""):
+        if filename.startswith("*"):
+            return self.search_globs(filename,dir)
+        dir_path = Path("/home",str(self.get_user()),dir)
+
         matched_files = []
-        for file_path in self.directory.rglob('*'):
-            if file_path.is_file() and query.lower() in file_path.name.lower():
-                matched_files.append(file_path)
+        for path in dir_path.rglob('*'):
+            if path.is_file() and path.name == filename and filename.startswith("*") is False:
+                matched_files.append({
+                    "path" : str(path),
+                    "ext":self.extract_ext(path.name),
+                    "filename":path.name or filename
+                    })
+        return matched_files
+    
+    def search_globs(self, pattern: str ,dir:str = ""):
+        dir_path = Path("/home",str(self.get_user()),dir)
+
+        matched_files = []
+        for path in dir_path.rglob(pattern):
+            if path.is_file():
+                matched_files.append({
+                    "path" : str(path),
+                    "ext":self.extract_ext(path.name),
+                    "filename":path.name 
+                    })
         return matched_files
     
 
-    def get_user(home_dir:str="home") -> str:
+    def get_user(self,home_dir:str="/home") -> str:
         dirs = Path(f"/{home_dir}").iterdir()
         users = [d.name for d in dirs if d.is_dir()]
-        return users
+        return users[0]
     
 
-if __name__ == "__main__":
-    # fs = FIleSearcher("/D")
-    # query = input("Enter search query: ")
-    # results = fs.search_files(query)
-    # if results:
-    #     print("Matched files:")
-    #     for file in results:
-    #         print(file)
-    # else:
-    #     print("No files matched the query.")
+
+## test runs
+
+
+# if __name__ == "__main__":
+#     fs = FIleSearcher()
+#     ddir = fs.search_files("test.c","D")
+#     print(ddir)
